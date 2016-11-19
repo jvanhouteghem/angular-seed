f7bb5a765a4980e9792e4df9f20e38a117732b17
jvanhouteghem <vanhouteghem.j@gmail.com>
2016-11-19 04:44:53 +0100

    Export CRUD from controller to provider

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -2,33 +2,27 @@
 
 app
     .controller('TodoListController', function ($scope, todosProvider) {
-        $scope.todos = todosProvider.getTodos();
+        // ----- Call provider -----
+        $scope.addTodo = function () {
+            todosProvider.addTodo($scope.getMaxTodoId() + 1, $scope.todoText, false);
+            $scope.todoText = '';
+        }
+        $scope.removeTodo = function (id) {
+            todosProvider.removeTodo(id);
+            $scope.todos = todosProvider.getTodos();
+        };
 
-        $scope.getMaxTodoId = function(){
+        // ----- -----
+        $scope.todos = todosProvider.getTodos();        
+
+        $scope.getMaxTodoId = function () {
             var result = 0;
             angular.forEach($scope.todos, function (todo) {
-                if (todo.id > result){
+                if (todo.id > result) {
                     result = todo.id;
                 }
             });
             return result;
-        };
-
-        $scope.addTodo = function () {
-            $scope.todos.push({ id: $scope.getMaxTodoId()+1, text: $scope.todoText, done: false });
-            $scope.todoText = '';
-        };
-
-        $scope.removeTodo = function(id){
-            var count = 0;
-            angular.forEach($scope.todos, function (todo) {
-                if (todo.id == id){
-                    $scope.todos.splice(count, 1);
-                }
-                count++;
-            });            
-            // Mise à jour de l'objet (actualisation de l'affichage)
-            $scope.todos = todosProvider.getTodos();
         };
 
         $scope.remaining = function () {
@@ -42,4 +36,4 @@ app
     .controller('todoCreate', function ($scope, categoryProvider) {
         $scope.categories = categoryProvider.getCategories();
     })
-;
+    ;
--- src/services/todosProvider.js
+++ src/services/todosProvider.js
@@ -6,7 +6,23 @@ var todos = [
 ];
 
 app.service('todosProvider', function () {
+    
     this.getTodos = function(){
         return todos;
     }
+
+    this.addTodo = function (id, text, done) {
+        todos.push({ id: id, text: text, done: done });
+    };
+
+    this.removeTodo = function(id){
+        var count = 0;
+        angular.forEach(todos, function (todo) {
+            if (todo.id == id){
+                todos.splice(count, 1);
+            }
+            count++;
+        });            
+    };
+
 })
